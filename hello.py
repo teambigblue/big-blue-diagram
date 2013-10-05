1baf7e0bd1f1d346a088fec60bda46b5ad3e0a21
Sinan Cepel <cepel.sinan@gmail.com>
2013-10-04 23:11:07 -0400

    Added basic html support to hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("main.html")
+    return render_template("templates/main.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8001)
+    app.run()
