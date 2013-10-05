1a3ff2cde31707c9b9420ab4454146d4a6c530ed
sinancepel <cepel.sinan@gmail.com>
2013-10-05 03:30:55 +0000

    Got html to work

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template,request,url_for,redirect,flash,session,Response
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def hello():
     return render_template("main.html")
 
