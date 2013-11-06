01f12c7c6de90b2a5eb4dce260279e2f187f0e0a
U-Nate\Nathaniel <Nathaniel@Nate.(none)>
2013-11-06 04:36:25 -0500

    Working on different link angles

--- static/js/basic.js
+++ static/js/basic.js
@@ -14,21 +14,32 @@ function crtRect(x,y) {
 }
 
 function crtArrow(startx,starty,endx,endy,dir,arrowcolor) {
-    if(dir = "down") {
-	var endadj = -arrowwidth*2
+    if(dir == "downr") {
+	var endadjv = -arrowwidth*2;
+	var endadjh = -arrowwidth*2;
     }
-    if(dir = "up") {
-	var endadj = arrowwidth*2
+    if(dir == "upr") {
+	var endadjv = arrowwidth*2;
+	var endadjh = -arrowwidth*2;	
     }
-    var slope = ((endy+endadj)-starty)/((endx+endadj)-startx);
+    if(dir == "downl") {
+	var endadjv = -arrowwidth*2;
+	var endadjh = arrowwidth*2;	
+    }
+    if(dir == "upl") {
+	var endadjv = arrowwidth*2;
+	var endadjh = arrowwidth*2;	
+    }
+    var slope = (endy-starty)/(endx-startx);
     var negslope = (-1)/slope;
-    var arrowpt1x = (arrowwidth/(Math.sqrt(1+(negslope*negslope))))+(endx+endadj);
-    var arrowpt1y = ((arrowpt1x-(endx+endadj))*negslope)+(endy+endadj);
-    var arrowpt2x = (-(arrowwidth)/(Math.sqrt(1+(negslope*negslope))))+(endx+endadj);
-    var arrowpt2y = ((arrowpt2x-(endx+endadj))*negslope)+(endy+endadj);
-    var arrowshaft = svg.line(startx,starty,endx+endadj,endy+endadj,{strokeWidth:arrowstroke,stroke:arrowcolor});
-    var arrowhead = svg.polygon([[endx+endadj,endy+endadj],[arrowpt1x,arrowpt1y],
-				 [endx,endy],[arrowpt2x,arrowpt2y],[endx+endadj,endy+endadj]],{fill:arrowcolor});
+    var arrowpt1x = (arrowwidth/(Math.sqrt(1+(negslope*negslope))))+(endx);
+    var arrowpt1y = ((arrowpt1x-(endx))*negslope)+(endy);
+    var arrowpt2x = (-(arrowwidth)/(Math.sqrt(1+(negslope*negslope))))+(endx);
+    var arrowpt2y = ((arrowpt2x-(endx))*negslope)+(endy);
+    var arrowshaft = svg.line(startx,starty,endx,endy,{strokeWidth:arrowstroke,stroke:arrowcolor});
+    //Figure out endpoints
+    var arrowhead = svg.polygon([[endx,endy],[arrowpt1x,arrowpt1y],
+				 [endx,endy],[arrowpt2x,arrowpt2y],[endx,endy]],{fill:arrowcolor});
 }
 
 function changeArrowWidth(x) {
@@ -40,23 +51,33 @@ function changeArrowStroke(x) {
 }
 
 function crtLink(node1,node2) {
-    var node1w = parseInt($(node1).css("width"));
-    var node1h = parseInt($(node1).css("height"));
-    var node1x = parseInt($(node1).css("top")) + node1w/2;
-    var node1y = parseInt($(node1).css("left")) - node1h/2;
-    var node2w = parseInt($(node2).css("width"));
-    var node2h = parseInt($(node2).css("height"));
-    var node2x = parseInt($(node2).css("top")) + node2w/2;
-    var node2y = parseInt($(node2).css("left")) - node2h/2;
+    var node1w = parseInt($(node1).attr("width"));
+    var node1h = parseInt($(node1).attr("height"));
+    var node1y = parseInt($(node1).attr("y")) + (node1h/2);
+    var node1x = parseInt($(node1).attr("x")) + (node1w/2);
+    var node2w = parseInt($(node2).attr("width"));
+    var node2h = parseInt($(node2).attr("height"));
+    var node2y = parseInt($(node2).attr("y")) + (node2h/2);
+    var node2x = parseInt($(node2).attr("x")) + (node2w/2);
     if (node1x > node2x) {
 	if (node1y > node2y) {
-	    if(Math.atan2(node1y-node2y,node1x-node2x) > PI/4)
-		crtarrow(node1x+node1w,node1y+node1h,node2x+node2w,node2y+node2h,"up","black");
-	    //else
-	//	crtarrow(node1x+node1
+	    if(Math.atan2(node1y-node2y,node1x-node2x) > Math.PI/4){
+		crtArrow(node1x,node1y-(node1h/2),node2x,node2y+(node2h/2),"upl","red");	
+	    }
+	    else {
+		crtArrow(node1x-(node1w/2),node1y,node2x+(node2w/2),node2y,"upl","red");
+	    }
+	}
+	else {
+	    if(Math.atan2(node1y-node2y,node1x-node2x) < -Math.PI/4){
+		crtArrow(node1x,node1y+(node1h/2),node2x,node2y-(node2h/2),"downl","red");	
+	    }
+	    else {
+		crtArrow(node1x-(node1h/2),node1y,node2x,node2y-(node2h/2),"downl","red");
+	    }
 	}
     }
 }
 
-function crtLink(x1,x2,y1,y2) {
-}
+//function crtLink(x1,x2,y1,y2) {
+//}
