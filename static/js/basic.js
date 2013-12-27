221bc361b655153a2bf5855de6370a7e0dd107d2
Nathaniel Biggs <biggs0125@gmail.com>
2013-12-26 21:38:25 -0500

    stuff

--- static/js/basic.js
+++ static/js/basic.js
@@ -2,6 +2,9 @@ var arrowwidth = 7;
 var arrowstroke = 2;
 var eventStack;
 var objectStack;
+var justdragged;
+var curxpos;
+var curypos;
 
 $(document).ready(function() {
     $("#canvas").svg();
@@ -9,7 +12,7 @@ $(document).ready(function() {
     svg = $("#canvas").svg('get');
     initCanvas();
     initEventStack();
-    readyToolbar();
+    initToolbar();
     initObjectStack();
 });
 
@@ -58,7 +61,7 @@ function initCanvas() {
     });
 }
 
-function readyToolbar() {
+function initToolbar() {
     $("#addrect").click(function() {
 	initEventStack();
 	eventStack.addingRect = true;
@@ -69,20 +72,27 @@ function readyToolbar() {
     });
 }
 
+function swapToolbarNode() {
+}
+
 function crtRect(x,y) {
     var newID = objectStack.rects.length.toString();
-    var curgroup = svg.group("rg"+newID);
-    var currect = svg.rect(curgroup,x,y,100,100,4,4);
+    $("#htmlcanvas").append("<textarea id='r"+newID+"' style='width:100px;height:100px;border-radius:4px;position:absolute;top:"+y+";left:"+x+";z-index:1;resize:none;'></textarea>");
+    currect = document.getElementById("r"+newID);
+    currect.active = false;
+    $(currect).attr('id',"r"+newID);
     $(currect).css('fill','white');
     $(currect).css('stroke','black');
-    $(currect).css('z-index','3');
-    $(currect).click(function() {
+    $(currect).click(function(event) {
+	if($(this).is('.ui-draggable-dragging')){
+	    return;
+	}
 	if (eventStack.addingLink1) {
 	    eventStack.addingLink2 = true;
-	    eventStack.linkNode = currect;
+	    eventStack.linkNode = event.target;
 	    eventStack.addingLink1 = false;
 	}
-	else if (eventStack.addingLink2 && eventStack.linkNode != currect) {
+	else if (eventStack.addingLink2 && eventStack.linkNode != event.target) {
 	    eventStack.addingLink2 = false;
 	    var linkobj = new Object();
 	    eventStack.addingLink2 = false;
@@ -92,43 +102,58 @@ function crtRect(x,y) {
 	    if(eventStack.linkPt) {
 		linkobj.pt1 = eventStack.linkPt;
 	    }
-	    linkobj.node2 = currect;
+	    linkobj.node2 = event.target;
 	    eventStack.linkPt = undefined;
 	    eventStack.linkNode = undefined;
 	    crtLink(linkobj);
 	}
+	else {
+	    if(!justdragged) {
+		$(event.target).draggable({'containment':$("#canvas"),
+				       'cancel':'textarea',
+					  });
+		event.target.focus();
+	    }
+	    else {
+		justdragged = false;
+	    }
+	}
     });
-    $(currect).attr('id',"r"+newID);
-    currect.text = svg.text(curgroup,x,y,"");
     currect.head = [];
     currect.tail = [];
-    currect.flashing = false;
-    currect.flashingInt;
-    $(currect).dblclick(function(event) {
-	if(event.target.text.flashing) {
-	    console.log("clearing Interval");
-	    clearInterval(event.target.flashingInt);
-	    event.target.flashing = false;
+    $(currect).draggable({'containment':$("#canvas"),
+			  'cancel':'',
+			  'delay':'50',
+			  'start':function(event,ui){
+			      curypos = event.pageY-$("#canvas").offset().top;
+			      curxpos = event.pageX-$("#canvas").offset().left;
+			  },
+			  'drag':function(event,ui){
+			      var dy=curypos-(event.pageY-$("#canvas").offset().top);
+			      var dx=curxpos-(event.pageX-$("#canvas").offset().left);
+			      curxpos = curxpos - dx;
+			      curypos = curypos - dy;
+			      if(!(dx==0 && dy==0)) {
+				  for(var i=0;i<event.target.head.length;i++) {
+				      changeArrowHead(event.target.head[i],dx,dy);
+				  }
+				  for(var i=0;i<event.target.tail.length;i++) {
+				      changeArrowTail(event.target.tail[i],dx,dy);   
+				  }
+			      }
+			  },
+			  'stop':function(){
+			      curxpos = 0;
+			      curypos = 0;
+			      justdragged = true;
+			  }
+			 });
+    $(currect).focusout(function(event) {
+	if (!justdragged) {
+	    $(event.target).draggable({'containment':$("#canvas"),'cancel':''});
 	}
 	else {
-	    eventStack.selectedRect = event.target;
-	    event.target.flashingInt = setInterval("flashingHandler",500);
-	    event.target.flashing = true;
-	}
-    });
-    $(currect).draggable({'containment':$("#canvas")});
-    $(currect).bind('drag', function(event, ui){
-	var dx = event.target.getAttribute('x') - ui.position.left;
-	var dy = event.target.getAttribute('y') - ui.position.top;
-	event.target.text.setAttribute('x',ui.position.left);
-	event.target.text.setAttribute('y',ui.position.top);
-        event.target.setAttribute('x', ui.position.left);
-	event.target.setAttribute('y', ui.position.top);
-	for(var i=0;i<event.target.head.length;i++) {
-	    changeArrowHead(event.target.head[i],dx,dy);
-	}
-	for(var i=0;i<event.target.tail.length;i++) {
-	    changeArrowTail(event.target.tail[i],dx,dy);
+	    justdragged = false;
 	}
     });
     objectStack.rects.push(currect);
@@ -147,6 +172,10 @@ function crtArrow(startx,starty,endx,endy,dir,arrowcolor) {
     if(dir == "upr") {
 	var endadj = -arrowwidth*2;
     }
+    var arrowang = Math.atan2(endy-starty,endx-startx);
+    var angpercent = (arrowang%45)/45;
+    var arrowstartdisp = angpercent*startx;
+    var arrowenddisp = angpercent*endx;
     var slope = (endy-starty)/(endx-startx);
     var negslope = (-1)/slope;
     var arrowpt0x = (endadj/(Math.sqrt(1+(slope*slope))))+(endx);
@@ -159,10 +188,6 @@ function crtArrow(startx,starty,endx,endy,dir,arrowcolor) {
     var arrowhead = svg.polygon([[arrowpt0x,arrowpt0y],[arrowpt1x,arrowpt1y],
 				 [endx,endy],[arrowpt2x,arrowpt2y],[arrowpt0x,arrowpt0y]],{'fill':arrowcolor});
     var arrow = new Object();
-    $(arrowhead).css("padding","50px");
-    $(arrowshaft).css("padding","50px");
-    $(arrowhead).css("z-index","3");
-    $(arrowshaft).css("z-index","3");
     var c1 = svg.circle(endx,endy,Math.abs(endadj/5),{'fill':'white','stroke':'black','strokeWidth':1});
     var c2 = svg.circle(startx,starty,Math.abs(endadj/5),{'fill':'white','stroke':'black','strokeWidth':1});
     $(c1).hide();
@@ -177,17 +202,92 @@ function crtArrow(startx,starty,endx,endy,dir,arrowcolor) {
     arrow.curEndX = endx;
     arrow.curEndY = endy;
     arrow.shaft = arrowshaft;
-    arrow.ang = Math.atan2(endy-starty,endx-startx);
+    arrow.ang = ang;
     arrow.c1 = c1;
     arrow.c2 = c2;
-    $(arrowhead).hover( function() {
-	$(c1).toggle();
-	$(c2).toggle();
-    });
+    arrow.headparent = null;
+    arrow.tailparent = null;
+    c1.arrow = arrow;
+    c2.arrow = arrow;
     $(arrowshaft).hover( function() {
 	$(c1).toggle();
 	$(c2).toggle();
     });
+    $(arrowhead).hover( function() {
+	$(c1).toggle();
+	$(c2).toggle();
+    });
+    $(c1).hover( function() {
+	$(c1).toggle();
+    });
+    $(c2).hover( function() {
+	$(c2).toggle();
+    });
+    $(c1).draggable({'containment':$("#canvas"),
+		     'start':function(event,ui){
+			 $(event.target).unbind("mouseenter");
+			 $(event.target).unbind("mouseleave");
+			 $(event.target.arrow.head).unbind("mouseenter");
+			 $(event.target.arrow.head).unbind("mouseleave");
+			 $(event.target.arrow.shaft).unbind("mouseenter");
+			 $(event.target.arrow.shaft).unbind("mouseleave");
+		     },
+		     'drag':function(event,ui){
+			 var dx=event.target.getAttribute('cx')-(event.pageX-$("#canvas").offset().left);
+			 var dy=event.target.getAttribute('cy')-(event.pageY-$("#canvas").offset().top);
+			 if(!(dx==0 && dy==0)) {
+			     changeArrowHead(event.target.arrow,dx,dy,true);
+			 }
+			 event.target.setAttribute('cx', event.pageX-$("#canvas").offset().left);
+			 event.target.setAttribute('cy', event.pageY-$("#canvas").offset().top);
+		     },
+		     'stop':function(event,ui){
+			 $(event.target).hover( function(event) {
+			     $(event.target).toggle();
+			 });
+			 $(event.target.arrow.shaft).hover( function() {
+			     $(event.target.arrow.c1).toggle();
+			     $(event.target.arrow.c2).toggle();
+			 });
+			 $(event.target.arrow.head).hover( function() {
+			     $(event.target.arrow.c1).toggle();
+			     $(event.target.arrow.c2).toggle();
+			 });
+		     }
+		    });
+    $(c2).draggable({'containment':$("#canvas"),
+		     'start':function(event,ui){
+			 $(event.target).unbind("mouseenter");
+			 $(event.target).unbind("mouseleave");
+			 $(event.target.arrow.head).unbind("mouseenter");
+			 $(event.target.arrow.head).unbind("mouseleave");
+			 $(event.target.arrow.shaft).unbind("mouseenter");
+			 $(event.target.arrow.shaft).unbind("mouseleave");
+
+		     },
+		     'drag':function(event,ui){
+			 var dx=event.target.getAttribute('cx')-(event.pageX-$("#canvas").offset().left);
+			 var dy=event.target.getAttribute('cy')-(event.pageY-$("#canvas").offset().top);
+			 if(!(dx==0 && dy==0)) {
+			     changeArrowTail(event.target.arrow,dx,dy,true);
+			 }
+			 event.target.setAttribute('cx', event.pageX-$("#canvas").offset().left);
+			 event.target.setAttribute('cy', event.pageY-$("#canvas").offset().top);
+		     },
+		     'stop':function(event){
+			 $(event.target).hover( function(event) {
+			     $(event.target).toggle();
+			 });
+			 $(event.target.arrow.shaft).hover( function() {
+			     $(event.target.arrow.c1).toggle();
+			     $(event.target.arrow.c2).toggle();
+			 });
+			 $(event.target.arrow.head).hover( function() {
+			     $(event.target.arrow.c1).toggle();
+			     $(event.target.arrow.c2).toggle();
+			 });
+		     }
+		    });
     objectStack.links.push(arrow);
     return arrow;
 }
@@ -200,7 +300,7 @@ function changeArrowStroke(x) {
     arrowstroke = x;
 }
 
-function changeArrowHead(arrow,dx,dy) {
+function changeArrowHead(arrow,dx,dy,isc1) {
     var origx = arrow.origEndX;
     var origy = arrow.origEndY;
     var curx = arrow.curEndX;
@@ -232,19 +332,23 @@ function changeArrowHead(arrow,dx,dy) {
     }
     arrow.shaft.setAttribute('x2',curx-dx);
     arrow.shaft.setAttribute('y2',cury-dy);
-    arrow.c1.setAttribute('cx',(curx-dx).toString());
-    arrow.c1.setAttribute('cy',(cury-dy).toString());
+    if(!isc1) {
+	arrow.c1.setAttribute('cx',(curx-dx).toString());
+	arrow.c1.setAttribute('cy',(cury-dy).toString());
+    }
 }
 
-function changeArrowTail(arrow,dx,dy) {
+function changeArrowTail(arrow,dx,dy,isc2) {
     var origx1 = arrow.shaft.getAttribute('x1');
     var origy1 = arrow.shaft.getAttribute('y1');
     var newx1 = parseInt(origx1)-parseInt(dx);
     var newy1 = parseInt(origy1)-parseInt(dy);
     arrow.shaft.setAttribute('x1',(newx1).toString());
     arrow.shaft.setAttribute('y1',(newy1).toString());
-    arrow.c2.setAttribute('cx',(newx1).toString());
-    arrow.c2.setAttribute('cy',(newy1).toString());
+    if(!isc2) {
+	arrow.c2.setAttribute('cx',(newx1).toString());
+	arrow.c2.setAttribute('cy',(newy1).toString());
+    }
     var theta = Math.atan2(arrow.curEndY-newy1,arrow.curEndX-newx1);
     theta = (arrow.ang*180/Math.PI) - (theta*180)/Math.PI;
     if(arrow.head.hasAttribute('transform')) {
@@ -286,23 +390,23 @@ function crtLink(obj) {
 	}
     }
     if (node1) {
-	var n1w = parseInt($(node1).attr("width"));
-	var n1h = parseInt($(node1).attr("height"));
-	var n1y = parseInt($(node1).attr("y")) + (n1h/2);
-	var n1x = parseInt($(node1).attr("x")) + (n1w/2);	
+	var n1w = parseInt($(node1).css("width"));
+	var n1h = parseInt($(node1).css("height"));
+	var n1y = parseInt($(node1).css("top")) + (n1h/2);
+	var n1x = parseInt($(node1).css("left")) + (n1w/2);	
     }
     if (node2) {
 	if (node1 || pt1) {
-	    var n2w = parseInt($(node2).attr("width"));
-	    var n2h = parseInt($(node2).attr("height"));
-	    var n2y = parseInt($(node2).attr("y")) + (n2h/2);
-	    var n2x = parseInt($(node2).attr("x")) + (n2w/2);
+	    var n2w = parseInt($(node2).css("width"));
+	    var n2h = parseInt($(node2).css("height"));
+	    var n2y = parseInt($(node2).css("top")) + (n2h/2);
+	    var n2x = parseInt($(node2).css("left")) + (n2w/2);
 	}
 	else { 
-	    var n1w = parseInt($(node2).attr("width"));
-	    var n1h = parseInt($(node2).attr("height"));
-	    var n1y = parseInt($(node2).attr("y")) + (n1h/2);
-	    var n1x = parseInt($(node2).attr("x")) + (n1w/2);
+	    var n1w = parseInt($(node2).css("width"));
+	    var n1h = parseInt($(node2).css("height"));
+	    var n1y = parseInt($(node2).css("top")) + (n1h/2);
+	    var n1x = parseInt($(node2).css("left")) + (n1w/2);
 	}
     }
     if (pt2) {
@@ -329,20 +433,24 @@ function crtLink(obj) {
 		if (n1y > y2) {
 		    if(Math.atan2(n1y-y2,n1x-x2) > Math.PI/4){
 			var newarw = crtArrow(n1x,n1y-(n1h/2),x2,y2,"upl","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x-(n1w/2),n1y,x2,y2,"upl","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(n1y-y2,n1x-x2) < -Math.PI/4){
 			var newarw = crtArrow(n1x,n1y+(n1h/2),x2,y2,"downl","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x-(n1h/2),n1y,x2,y2,"downl","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		}
@@ -351,20 +459,24 @@ function crtLink(obj) {
 		if (n1y > y2) {
 		    if(Math.atan2(y2-n1y,x2-n1x) > -Math.PI/4){
 			var newarw = crtArrow(n1x,n1y-(n1h/2),x2,y2,"upr","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x,n1y-(n1h/2),x2,y2,"upr","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(y2-n1y,x2-n1x) < Math.PI/4){
 			var newarw = crtArrow(n1x+(n1w/2),n1y,x2,y2,"downr","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x,n1y+(n1h/2),x2,y2,"downr","red");
+			newarw.tailparent = node2;
 			node2.tail.push(newarw);
 		    }
 		}
@@ -377,24 +489,32 @@ function crtLink(obj) {
 		if (n1y > n2y) {
 		    if(Math.atan2(n1y-n2y,n1x-n2x) > Math.PI/4){
 			var newarw = crtArrow(n1x,n1y-(n1h/2),n2x,n2y+(n2h/2),"upl","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x-(n1w/2),n1y,n2x+(n1w/2),n2y,"upl","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(n1y-n2y,n1x-n2x) < -Math.PI/4){
 			var newarw = crtArrow(n1x,n1y+(n1h/2),n2x,n2y-(n2h/2),"downl","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x-(n1h/2),n1y,n2x,n2y-(n2h/2),"downl","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
@@ -403,24 +523,32 @@ function crtLink(obj) {
 		if (n1y > n2y) {
 		    if(Math.atan2(n2y-n1y,n2x-n1x) > -Math.PI/4){
 			var newarw = crtArrow(n1x,n1y-(n1h/2),n2x-(n2w/2),n2y,"upr","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x,n1y-(n1h/2),n2x,n2y+(n2h/2),"upr","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(n2y-n1y,n2x-n1x) < Math.PI/4){
 			var newarw = crtArrow(n1x+(n1w/2),n1y,n2x-(n2w/2),n2y,"downr","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(n1x,n1y+(n1h/2),n2x,n2y-(n2h/2),"downr","red");
+			newarw.tailparent = node1;
 			node1.tail.push(newarw);
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
@@ -431,20 +559,24 @@ function crtLink(obj) {
 		if (y1 > n2y) {
 		    if(Math.atan2(y1-n2y,x1-n2x) > Math.PI/4){
 			var newarw = crtArrow(x1,y1,n2x,n2y+(n2h/2),"upl","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(x1,y1,n2x+(n2w/2),n2y,"upl","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(y1-n2y,x1-n2x) < -Math.PI/4){
 			var newarw = crtArrow(x1,y1,n2x,n2y-(n2h/2),"downl","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(x1,y1,n2x+(n2w/2),n2y,"downl","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
@@ -453,20 +585,24 @@ function crtLink(obj) {
 		if (y1 > n2y) {
 		    if(Math.atan2(n2y-y1,n2x-x1) > -Math.PI/4){
 			var newarw = crtArrow(x1,y1,n2x-(n2w/2),n2y,"upr","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(x1,y1,n2x,n2y+(n2h/2),"upr","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
 		else {
 		    if(Math.atan2(n2y-y1,n2x-x1) < Math.PI/4){
 			var newarw = crtArrow(x1,y1,n2x-(n2w/2),n2y,"downr","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		    else {
 			var newarw = crtArrow(x1,y1,n2x,n2y-(n2h/2),"downr","red");
+			newarw.headparent = node2;
 			node2.head.push(newarw);
 		    }
 		}
@@ -475,17 +611,3 @@ function crtLink(obj) {
     }
 }
 
-function flashingHandler(event) {
-    if(event.target.text.on) {
-	var curtext = $(event.target.text).text();
-	var newtext = curtext.substring(0,curtext.length-1);
-	$(event.target.text).text(newtext);
-	console.log("off");
-    }
-    else {
-	var curtext = $(event.target.text).text();
-	var newtext = curtext.concat('|');
-	$(event.target.text).text(newtext);
-	console.log("on");
-    }
-}
